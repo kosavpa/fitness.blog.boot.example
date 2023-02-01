3a30f13ee566b74e916d27700e1c8abfc10d9088
kosavpa <kosavpa@yandex.ru>
2023-02-01 19:28:18 +0400

    swiming

---
+++ src/main/resources/temp/menu_animation.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  document.querySelector(".drop_btn")
+  .addEventListener("click", function () {
+    document.getElementById("drop_menu")
+      .classList
+      .toggle("show")
+  }); 
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches("drop_btn")) {
+    var menu_list = document.getElementById("drop_menu");
+    
+    if (menu_list.classList.contains("show")) {
+      menu_list
+        .classList
+        .remove("show");
+    }
+  }
+}
