3fa93f279438c912163a8f353abee1ab7dc452a4
kosavpa <kosavpa@yandex.ru>
2023-02-01 19:19:40 +0400

    swiming

---
+++ src/main/java/com/kosavpa/first/boot/example/dao/entity/post/ProposedArticle.java
@@ -0,0 +1,40 @@
+package com.kosavpa.first.boot.example.dao.entity.post;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+//@Data
+//@Entity
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Table(name = "proposed_blog_article")
+public class ProposedArticle {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "date")
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+    @Column(name = "anons")
+    private String anons;
+    @Column(name = "full_text", columnDefinition = "varchar(1000)")
+    private String fullText;
+    @Transient
+    private String formattedDate;
+
+    public void setFormattedPublicationDate(String format){
+        formattedDate = new SimpleDateFormat(format).format(publicationDate);
+    }
+}
